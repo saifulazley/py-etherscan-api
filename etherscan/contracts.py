a047e334cfd28680aa6d083bad0b08940f61b620
saifulazley <saifulazley79@gmail.com>
2025-01-02 18:59:43 +0800

    apikeysas

--- etherscan/contracts.py
+++ etherscan/contracts.py
@@ -2,7 +2,7 @@ from .client import Client
 
 
 class Contract(Client):
-    def __init__(self, address=Client.dao_address, api_key='YourApiKeyToken'):
+    def __init__(self, address=Client.0x757B70675cAF0E00a9ba25352ea8BAD8700c3A08, api_key='JRUIB4MAE329D5BM3NA78UAE1UXPKMPPK8'):
         Client.__init__(self, address=address, api_key=api_key)
         self.url_dict[self.MODULE] = 'contract'
 
